e75f9dc74f4e6c60e2ef680f932fed930f13f71a
ohba_yuichi <nepinepi984@gmail.com>
2017-06-20 11:55:57 +0900

    JSON変換用classの実装

---
+++ app/src/main/java/com/example/yuichi_oba/ostraca/models/StudentListItem.java
@@ -0,0 +1,8 @@
+package com.example.yuichi_oba.ostraca.models;
+
+/**
+ * Created by Yuichi-Oba on 2017/06/20.
+ */
+
+public class StudentListItem {
+}
---
+++ app/src/main/java/com/example/yuichi_oba/ostraca/models/V_stu_attend_status.java
@@ -0,0 +1,17 @@
+package com.example.yuichi_oba.ostraca.models;
+
+import java.io.Serializable;
+
+/**
+ * Created by Yuichi-Oba on 2017/06/20.
+ */
+
+public class V_stu_attend_status implements Serializable {
+    public String stu_id;
+    public String stu_name;
+    public String sub_id;
+    public String sub_name;
+    public String attend;
+    public String total_lesson;
+    public String attend_rate;
+}
